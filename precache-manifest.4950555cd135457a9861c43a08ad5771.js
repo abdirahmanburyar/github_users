b73890e461e9aab6d79418760a63c9ad14c59bf6
abdirahman buryar <buryar313@gmail.com>
2020-05-08 12:29:51 -0700

    Updates

---
+++ precache-manifest.4950555cd135457a9861c43a08ad5771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "062e8b691f5b9fa7fa97",
+    "url": "/github_users/static/css/main.7e629ca7.chunk.css"
+  },
+  {
+    "revision": "062e8b691f5b9fa7fa97",
+    "url": "/github_users/static/js/main.2f22f727.chunk.js"
+  },
+  {
+    "revision": "98cb0097e28f8e5572f4",
+    "url": "/github_users/static/js/runtime~main.b7024680.js"
+  },
+  {
+    "revision": "278a8f1c1443c4d2dc48",
+    "url": "/github_users/static/js/2.7eb4b854.chunk.js"
+  },
+  {
+    "revision": "9dca4032333854e3f79ce627bdeb2b09",
+    "url": "/github_users/index.html"
+  }
+];
